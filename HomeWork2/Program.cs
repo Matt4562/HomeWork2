bfae8b6de94d6c3a3a2aa73e347d500e7f21bd2c
Matt4562 <matissm@outlook.com>
2022-04-17 21:13:37 +0300

    Add project files.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+string greeting = "Kā Tevi sauc?";
+
+Console.WriteLine(greeting);
+
+string userName = Console.ReadLine();
+
+String userGreating = "Sveiks, " + userName + "!";
+Console.WriteLine(userGreating);
+
+
